37a9563b90de68d20a7bbbc573e18ccf1fea34be
st084331 <71183199+st084331@users.noreply.github.com>
2021-10-25 14:04:25 +0300

    ура заработал

--- TeleBot.py
+++ TeleBot.py
@@ -21,7 +21,6 @@ def start_command(message):
 
 @bot.message_handler(commands=['language', 'start'])
 def select_language(message):
-    print(message.from_user.id)
     markup = types.InlineKeyboardMarkup(row_width=2)
     ENG_Button = types.InlineKeyboardButton("English", callback_data='ENG')
     RUS_Button = types.InlineKeyboardButton("Русский", callback_data='RUS')
@@ -31,7 +30,7 @@ def select_language(message):
 
 @bot.message_handler(commands=['status', 'start'])
 def select_foreigner(message):
-    if get_language(message.from_user.id) == "RUS":
+    if get_language(message.chat.id) == "RUS":
         markup2 = types.InlineKeyboardMarkup(row_width=2)
         Not_Foreigner = types.InlineKeyboardButton("Гражданин РФ", callback_data='NO')
         Foreigner = types.InlineKeyboardButton("Иностранец", callback_data='YES')
@@ -42,8 +41,7 @@ def select_foreigner(message):
 @bot.message_handler(commands=['degree', 'start'])
 def select_degree(message):
     markup3 = types.InlineKeyboardMarkup(row_width=3)
-    print(message.from_user.id)
-    if get_language(message.from_user.id) == "ENG":
+    if get_language(message.chat.id) == "ENG":
         Bachelor = types.InlineKeyboardButton("Bachelor", callback_data='Bachelor')
         Master = types.InlineKeyboardButton("Master", callback_data='Master')
         PhD = types.InlineKeyboardButton("Postgraduate", callback_data='PhD')
@@ -51,7 +49,7 @@ def select_degree(message):
         markup3.add(Bachelor, Master, PhD, Residency)
         bot.send_message(message.chat.id, "Please select the degree of education you are interested in.",
                          reply_markup=markup3)
-    elif get_language(message.from_user.id) == "RUS":
+    elif get_language(message.chat.id) == "RUS":
         Bachelor = types.InlineKeyboardButton("Бакалавриат", callback_data='Bachelor')
         Master = types.InlineKeyboardButton("Магистратура", callback_data='Master')
         PhD = types.InlineKeyboardButton("Аспирантура", callback_data='PhD')
@@ -70,40 +68,39 @@ def callback_inline(call):
         if call.data == 'ENG':
             bot.send_message(call.message.chat.id, 'Fine! To change the language, enter the command /language')
             bot.send_message(call.message.chat.id, "You can ask me any question that interests you!")
-            change_language(call.from_user.id, call.data, "YES", get_degree(call.from_user.id))
+            change_language(call.message.chat.id, call.data, "YES", get_degree(call.message.chat.id))
             bot.edit_message_text(text="Please select a language", chat_id=call.message.chat.id,
                                   message_id=call.message.message_id, reply_markup=None)
-            print(get_language(call.from_user.id))
-            if get_degree(call.from_user.id) == "None":
+            if get_degree(call.message.chat.id) == "None":
                 select_degree(call.message)
         elif call.data == 'RUS':
             bot.send_message(call.message.chat.id, 'Хорошо! Чтобы сменить язык, введите команду /language')
-            change_language(call.from_user.id, call.data, get_foreigner(call.from_user.id),
-                            get_degree(call.from_user.id))
+            change_language(call.message.chat.id, call.data, get_foreigner(call.message.chat.id),
+                            get_degree(call.message.chat.id))
             bot.edit_message_text(text="Пожалуйста, выберите язык", chat_id=call.message.chat.id,
                                   message_id=call.message.message_id, reply_markup=None)
             select_foreigner(call.message)
         elif call.data == "YES" or call.data == "NO":
             bot.send_message(call.message.chat.id, 'Хорошо! Чтобы сменить статус, введите команду /status')
-            change_foreigner(call.from_user.id, get_language(call.from_user.id), call.data,
-                             get_degree(call.from_user.id))
+            change_foreigner(call.message.chat.id, get_language(call.message.chat.id), call.data,
+                             get_degree(call.message.chat.id))
             bot.edit_message_text(text="Пожалуйста, выберите свой статус.", chat_id=call.message.chat.id,
                                   message_id=call.message.message_id, reply_markup=None)
-            if get_degree(call.from_user.id) == "None":
+            if get_degree(call.message.chat.id) == "None":
                 select_degree(call.message)
         elif call.data == "Bachelor" or call.data == "Master" or call.data == "PhD" or call.data == "Residency":
-            if get_language(call.from_user.id) == "RUS":
+            if get_language(call.message.chat.id) == "RUS":
                 bot.send_message(call.message.chat.id,
                                  "Хорошо! Чтобы сменить интересующую степень образования, введите команду /degree")
-                change_degree(call.from_user.id, get_language(call.from_user.id), get_foreigner(call.from_user.id),
+                change_degree(call.message.chat.id, get_language(call.message.chat.id), get_foreigner(call.message.chat.id),
                               call.data)
                 bot.edit_message_text(text="Пожалуйста, выберите интересующую Вас степень образования.",
                                       chat_id=call.message.chat.id,
                                       message_id=call.message.message_id, reply_markup=None)
-            elif get_language(call.from_user.id) == "ENG":
+            elif get_language(call.message.chat.id) == "ENG":
                 bot.send_message(call.message.chat.id,
                                  "Fine! To change the degree of education you are interested in, enter the command /degree")
-                change_degree(call.from_user.id, get_language(call.from_user.id), get_foreigner(call.from_user.id),
+                change_degree(call.message.chat.id, get_language(call.message.chat.id), get_foreigner(call.message.chat.id),
                               call.data)
                 bot.edit_message_text(text="Please select the degree of education you are interested in.",
                                       chat_id=call.message.chat.id,
@@ -112,10 +109,10 @@ def callback_inline(call):
 
 @bot.message_handler(content_types=['text'])
 def answers(message):
-    if get_language(message.from_user.id) == "ENG":
+    if get_language(message.chat.id) == "ENG":
         bot.send_message(message.chat.id, chat_AI(message.text))
     else:
-        if get_foreigner(message.from_user.id) == "NO":
+        if get_foreigner(message.chat.id) == "NO":
             bot.send_message(message.chat.id, chat_AI_ru(message.text))
         else:
             bot.send_message(message.chat.id, chat_AI_ru_foreingner(message.text))
